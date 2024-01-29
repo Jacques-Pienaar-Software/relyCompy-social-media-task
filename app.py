c0370b5498ff7b607dbb77d50eff39f5393a061e
Jacques Pienaar <jacquespienaar.software@gmail.com>
2024-01-29 16:53:02 +0200

    Final touches

--- app.py
+++ app.py
@@ -12,11 +12,12 @@ api_extensions = ["twitter", "facebook", "instagram"]
 @app.route("/")
 async def social_network_activity():
     # TODO: your code here
-    async with aiohttp.ClientSession() as session:
-        response = await fetch_all(session, api_extensions)
+    
+    response = await fetch_all(api_extensions)
+
     return response;
 
-async def fetch(s, urls):
+async def fetch(s, url):
     '''
     Create an instance of aiohttp fetch to get a single endpoint.
     Exception handling also happens here on each individual isntance created.
@@ -24,11 +25,11 @@ async def fetch(s, urls):
     Parameters:
     -----------
     s: An aiohttp client session object
-    urls: The url extensions that we want to target 
+    url: The url extensions that we want to target 
     '''
 
     try:
-        async with s.get(f'{BASE_URL}/{urls}') as r:
+        async with s.get(f'{BASE_URL}/{url}') as r:
             if r.status != 200:
                 r.raise_for_status()
             return await r.text()
@@ -39,23 +40,22 @@ async def fetch(s, urls):
         print('Connection Error', str(e)) # A more in-depth logger would be implemented here
         return []
 
-async def fetch_all(s, urls):
+async def fetch_all(urls):
     '''
     Create a task for each API enpoint and call them concurrently 
     using asyncio.gather()
 
     Parameters:
     -----------
-    s: A aiohttp session
     urls: The url extensions that we want to target 
     '''
-
-    api_responses = []
-    for url in urls:
-        res = asyncio.create_task(fetch(s, url))
-        api_responses.append(res)
-    res = await asyncio.gather(*api_responses)
-    
+    async with aiohttp.ClientSession() as session:
+        api_responses = []
+        for url in urls:
+            res = asyncio.create_task(fetch(session, url))
+            api_responses.append(res)
+        res = await asyncio.gather(*api_responses)
+        
     return get_aggregate_data(urls, res)
 
 def get_aggregate_data(urls, api_responses):
